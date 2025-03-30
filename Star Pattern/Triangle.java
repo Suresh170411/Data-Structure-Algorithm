b699607d37f8c4935c6953ee1b155bc263a2b3c3
suresh170411 <suresh.20fk@gmail.com>
2025-03-30 07:51:33 +0530

    Triange star pattern2

---
+++ Star Pattern/Triangle.java
@@ -0,0 +1,35 @@
+public class Triangle {
+    public static void main(String[] args) {
+        pattern2_brute_force(7);
+    }
+
+    // T.C --> O(n2)
+    public static void pattern2_brute_force(int levels){
+        for (int i=1; i<=levels; i++){
+            for (int j=1; j<=levels-i; j++){
+                System.out.print(" ");
+            }
+
+            for (int j=1; j<=(i*2)-1; j++){
+                System.out.print("*");
+            }
+
+            for (int j=1; j<=levels-i; j++){
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+
+    // T.C --> O(n)
+    public static void pattern2_optimized(int levels){
+        for (int i = 1; i <= levels; i++) {
+            // Calculate stars and spaces
+            String stars = "*".repeat(2 * i - 1);
+            String spaces = "-".repeat(levels - i);
+
+            // Generate and print pattern row
+            System.out.println(spaces + stars + spaces);
+        }
+    }
+}
