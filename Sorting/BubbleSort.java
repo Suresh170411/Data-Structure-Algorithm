9bf5050be7c6d68031a2a9e7278407ae57a09064
Suresh Kumar Mallik <suresh.20fk@gmail.com>
2025-03-31 18:14:38 +0530

    Update BubbleSort.java

--- Sorting/BubbleSort.java
+++ Sorting/BubbleSort.java
@@ -9,12 +9,18 @@ public class BubbleSort {
     }
 
     public static void bubbleSortTechnique (int arr []){
+        boolean swapped;
         int n = arr.length;
         for (int i=0; i<n-1; i++){
+            swapped = false;
             for (int j=0; j<n-i-1; j++){
                 if (arr[j] > arr[j+1]){
                     swap(arr, j, j+1);
+                    swapped = true;
                 }
+            }
+            if (!swapped){
+                break;
             }
         }
         System.out.println(Arrays.toString(arr));
