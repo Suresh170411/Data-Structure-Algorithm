3eb4307cae91eb89b48224b8da3524894c5406b9
Suresh Kumar Mallik <suresh.20fk@gmail.com>
2025-03-31 17:42:29 +0530

    Create BubbleSort.java

---
+++ Sorting/BubbleSort.java
@@ -0,0 +1,28 @@
+package Sorting;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        int arr [] = {3,5,1,9,8,2};
+        bubbleSortTechnique(arr);
+    }
+
+    public static void bubbleSortTechnique (int arr []){
+        int n = arr.length;
+        for (int i=0; i<n-1; i++){
+            for (int j=0; j<n-i-1; j++){
+                if (arr[j] > arr[j+1]){
+                    swap(arr, j, j+1);
+                }
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+
+    private static void swap(int arr [], int i, int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
