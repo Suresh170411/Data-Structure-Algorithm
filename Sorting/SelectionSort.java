db9cfeb6cacd026618cc090424c64fefb1a7dacc
Suresh Kumar Mallik <suresh.20fk@gmail.com>
2025-03-31 18:14:43 +0530

    Create SelectionSort.java

---
+++ Sorting/SelectionSort.java
@@ -0,0 +1,30 @@
+package Sorting;
+
+import java.util.Arrays;
+
+public class SelectionSort {
+    public static void main(String[] args) {
+        int arr [] = {2,1,8,5,9,4};
+        selectionSortTechnique(arr);
+    }
+
+    public static void selectionSortTechnique(int arr []){
+        int n = arr.length;
+        for (int i=0; i<n-1; i++){
+            int smallest = i;
+            for (int j=i+1; j<n; j++){
+                if (arr[smallest] > arr[j]){
+                    smallest = j;
+                }
+            }
+            swap(arr, smallest, i);
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+
+    private static void swap(int arr [], int i, int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
